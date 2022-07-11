734e1e6cdb6f27372367cb77e98c67bb110c4049
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2022-07-11 21:01:37 +0700

    Project 1 done

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="my-navbar">
+      <nav className="my-nav">
+        <img src="logo192.png" alt="ReactJS logo 192x192px" />
+        <h3>ReactFacts</h3>
+        <div>React Course - Project 1</div>
+      </nav>
+    </header>
+  );
+}
